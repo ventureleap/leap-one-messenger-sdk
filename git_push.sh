8697dff4b17de898e720908e6f018fadb17117fe
GitHub Action <action@github.com>
2021-06-17 15:02:51 +0000

    Messenger Service commit hash: ec0162e

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Messenger Service commit hash: a5880ca"
+    release_note="Messenger Service commit hash: ec0162e"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
